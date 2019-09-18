67163a0c54bd798d1daca7b652de783b50077b46
guilhermemarcelino25 <guilhermemarcelinodasilva@hotmail.com>
2019-09-18 11:02:21 -0300

    Updates

---
+++ precache-manifest.24d8d9d6e1c3d1fcc7a3e831025e36d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e764e0e1b839774daff8bf4ea8fa94aa",
+    "url": "/copadubo-react/index.html"
+  },
+  {
+    "revision": "7bbca62150a6b2d8fad9",
+    "url": "/copadubo-react/static/css/main.ebd07c9d.chunk.css"
+  },
+  {
+    "revision": "a632add6a450ffa30720",
+    "url": "/copadubo-react/static/js/2.4e4f6698.chunk.js"
+  },
+  {
+    "revision": "7bbca62150a6b2d8fad9",
+    "url": "/copadubo-react/static/js/main.cdd0b7f0.chunk.js"
+  },
+  {
+    "revision": "b03c32d58312f69720de",
+    "url": "/copadubo-react/static/js/runtime~main.89ccb127.js"
+  }
+]);
